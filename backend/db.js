a7a091c5bba8624ab45d530513bc4a51c59614a6
subbu-475 <129367368+subbu-475@users.noreply.github.com>
2024-04-21 18:44:56 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB=async ()=>{
     try{
-        await mongoose.connect(process.env.online_string);
+        await mongoose.connect("mongodb+srv://personalcse475:Subbucse475@cluster0.1aikfy0.mongodb.net/femi");
         console.log("data base connected successfully");
     }
     catch(err){
